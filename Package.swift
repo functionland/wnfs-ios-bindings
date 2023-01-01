59e9fc7a2b1c2dbc85e343ebe403d58a0a5829b7
Homayoun <homayoun@Homayouns-MacBook-Pro.local>
2023-01-01 14:59:09 +0330

    add swift bundle

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
             .binaryTarget(
                 name: "Wnfs",
                 url: "https://github.com/functionland/wnfs-ios/bundle.zip",
-                checksum: ""),
+                checksum: "4f5a6c3486c1292b13dc814948d32b95da531b5c0aacd7a86bfdcf5baeb10a6b"),
             .testTarget(
                 name: "WnfsSwiftTests",
                 dependencies: ["WnfsSwift"]),
